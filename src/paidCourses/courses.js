d1a5309604657b42099babe8d653e445771b7f8a
AMDARIS\ionel.vidrighin <ionel.vidrighin@amdaris.com>
2021-11-04 11:19:04 +0200

    updates in json file

--- src/paidCourses/courses.js
+++ src/paidCourses/courses.js
@@ -4,7 +4,7 @@ module.exports = [
     courseLogo: 'https://static.wixstatic.com/media/9a56fd_417962dfd79747ccafad04e179cef172~mv2.png/v1/fill/w_236,h_234,al_c,q_85,usm_0.66_1.00_0.01/curs%20gdpr%20NOU.webp',
     courseLink: 'link here',
     courseTitle: 'Bazele GDPR & SECURITATEA INFORMAȚIEI',
-    courseDate: '19 martie 2022 18:30',
+    courseDate: '2021-11-03T18:30',
     courseLocation: 'Online Zoom',
     courseTrainer: 'Dummy trainer',
     courseActive: true
@@ -14,7 +14,7 @@ module.exports = [
     courseLogo: 'https://static.wixstatic.com/media/9a56fd_5e645613927c443b893a056081473cbb~mv2.png/v1/fill/w_236,h_234,al_c,q_85,usm_0.66_1.00_0.01/16.webp',
     courseLink: 'link here',
     courseTitle: 'Bazele HR (Recrutare & Selecție)',
-    courseDate: '27 noiembrie 2021 18:30',
+    courseDate: '2021-11-04T18:30',
     courseLocation: 'Online Zoom',
     courseTrainer: 'Dummy trainer',
     courseActive: true
@@ -24,7 +24,7 @@ module.exports = [
     courseLogo: 'https://static.wixstatic.com/media/9a56fd_5f0b8f178daa4adc8ec6219d76e80d9a~mv2.png/v1/fill/w_236,h_234,al_c,q_85,usm_0.66_1.00_0.01/curs%20sm%20NOU.webp',
     courseLink: 'link here',
     courseTitle: 'Bazele SOCIAL MEDIA - Strategie de vanzare si Copywriting',
-    courseDate: '6 decembrie 2022 18:30',
+    courseDate: '2021-11-08T18:30',
     courseLocation: 'Online Zoom',
     courseTrainer: 'Dummy trainer',
     courseActive: false
@@ -34,7 +34,7 @@ module.exports = [
     courseLogo: 'https://static.wixstatic.com/media/9a56fd_eacb2dc045004e6dac6469565b1e59fb~mv2.png/v1/fill/w_236,h_234,al_c,q_85,usm_0.66_1.00_0.01/12.webp',
     courseLink: 'link here',
     courseTitle: 'Bazele EMPLOYER BRANDING (Imagine de Angajator)',
-    courseDate: '16 decembrie 2022 18:30',
+    courseDate: '2021-11-09T18:30',
     courseLocation: 'Online Zoom',
     courseTrainer: 'Dummy trainer',
     courseActive: true
@@ -44,7 +44,7 @@ module.exports = [
     courseLogo: 'https://static.wixstatic.com/media/9a56fd_eacb2dc045004e6dac6469565b1e59fb~mv2.png/v1/fill/w_236,h_234,al_c,q_85,usm_0.66_1.00_0.01/12.webp',
     courseLink: 'link here',
     courseTitle: 'Bazele VANZARILOR - vanzari fata in fata, la telefon si in scris',
-    courseDate: '16 decembrie 2022 18:30',
+    courseDate: '2021-11-18T18:30',
     courseLocation: 'Online Zoom',
     courseTrainer: 'Dummy trainer',
     courseActive: true
@@ -54,7 +54,7 @@ module.exports = [
     courseLogo: 'https://static.wixstatic.com/media/9a56fd_eacb2dc045004e6dac6469565b1e59fb~mv2.png/v1/fill/w_236,h_234,al_c,q_85,usm_0.66_1.00_0.01/12.webp',
     courseLink: 'link here',
     courseTitle: 'Bazele DESIGN GRAFIC - UX & UI',
-    courseDate: '16 decembrie 2022 18:30',
+    courseDate: '2021-11-16T18:30',
     courseLocation: 'Online Zoom',
     courseTrainer: 'Dummy trainer',
     courseActive: true
@@ -64,7 +64,7 @@ module.exports = [
     courseLogo: 'https://static.wixstatic.com/media/9a56fd_eacb2dc045004e6dac6469565b1e59fb~mv2.png/v1/fill/w_236,h_234,al_c,q_85,usm_0.66_1.00_0.01/12.webp',
     courseLink: 'link here',
     courseTitle: 'Bazele MARKETING-ului - Digital Marketing si Offline Marketing',
-    courseDate: '16 decembrie 2022 18:30',
+    courseDate: '2021-11-12T18:30',
     courseLocation: 'Online Zoom',
     courseTrainer: 'Dummy trainer',
     courseActive: true
@@ -74,7 +74,7 @@ module.exports = [
     courseLogo: 'https://static.wixstatic.com/media/9a56fd_eacb2dc045004e6dac6469565b1e59fb~mv2.png/v1/fill/w_236,h_234,al_c,q_85,usm_0.66_1.00_0.01/12.webp',
     courseLink: 'link here',
     courseTitle: 'Bazele ATRACTIVITĂȚII în fața angajatorilor și clienților',
-    courseDate: '16 decembrie 2022 18:30',
+    courseDate: '2021-11-15T18:30',
     courseLocation: 'Online Zoom',
     courseTrainer: 'Dummy trainer',
     courseActive: true
